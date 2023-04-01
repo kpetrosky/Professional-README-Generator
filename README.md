de0e91641d2282b5af50f47d357eb4828cd3e37e
kpetrosky <kpetrosky36@gmail.com>
2023-04-01 12:35:03 -0700

    figured out how to make the get information to print to the read.md file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Professional-README-Generator
+# My Project
+
+A short description of my project
+
+## Installation
+
+npm install
+
+## Usage
+
+node index.js
+
+## Contributing
+
+Contributions welcome!
+
+## Tests
+
+npm test
+
+## License
+
+MIT License
+
+2023  Name
--- index.js
+++ index.js
@@ -40,103 +40,101 @@ const [markup, style, scripting] = languages;
 console.log(markup, style, scripting);
 console.log(markup);
 // TODO: Create an array of questions for user input
-const prompt = [{
-    type: 'input',
-    message: 'What is your user name?',
-    name: 'username',
-},
-{
-    type: 'password',
-    message: 'What is your password?',
-    name: 'password',
-},
-{
-    type: 'password',
-    message: 'Re-enter password to confirm:',
-    name: 'confirm',
-},];
 
-if ((response) =>
-    response.confirm === response.password
-        ? console.log('Success!')
-        : console.log('You forgot your password already?!')
-);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data)
-const fs = require('fs')
 
-fs.writeFile('README.md', readmeContent, err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('README file generated successfully!');
-    }
-});
+const fs = require('fs');
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`${fileName} file generated successfully!`);
+        }
+    });
+}
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`${fileName} file generated successfully!`);
+        }
+    });
+}
+
 function readmeContent(title, description, installation, usage, contributing, tests, license) {
     // Construct the content of the README file using the provided parameters
     const readmeContent = `# ${title}
 
 ${description}
 
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [License](#license)
-
 ## Installation
+
 ${installation}
 
 ## Usage
+
 ${usage}
 
 ## Contributing
+
 ${contributing}
 
 ## Tests
+
 ${tests}
 
 ## License
-This project is licensed under the ${license} license.
 
----
+${license}
 
 ${new Date().getFullYear()}  Name`;
+    
+    // Return the generated content
+    return readmeContent;
 }
-// Write the content to a README file in the current directory
+
+// Call the functions to generate and write the README file
+const content = readmeContent('My Project', 'A short description of my project', 'npm install', 'node index.js', 'Contributions welcome!', 'npm test', 'MIT License');
+writeToFile('README.md', content);
+
+
 
 
 // TODO: Create a function to initialize app
 function init() {
-const inquirer = require('inquirer');
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'Title of README',
-      message: 'What is the name of your README?', //recommended to use a name that represents the project. I.E. Professional-README-generator.
-    },
-    {
-      type: 'input',
-      name: 'describe',
-      message: 'Please decribe your application?', //recommended to be clear and use screen shots as needed
-      
-    },
-    {
-      type: 'input',
-      name: 'author',
-      message: 'Who wrote the README?', // include all contributors
-    }
-  ])
-  .then((response) => {
-    fs.writeFile("input.json", JSON.stringify(response), function (err) {
-      if (err) {
-        console.log('Success!')
-      }
-    });
-  });
+    const inquirer = require('inquirer');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'Title of README',
+                message: 'What is the name of your README?', //recommended to use a name that represents the project. I.E. Professional-README-generator.
+            },
+            {
+                type: 'input',
+                name: 'describe',
+                message: 'Please decribe your application?', //recommended to be clear and use screen shots as needed
+
+            },
+            {
+                type: 'input',
+                name: 'author',
+                message: 'Who wrote the README?', // include all contributors
+            }
+        ])
+        .then((response) => {
+            fs.writeFile("input.json", JSON.stringify(response), function (err) {
+                if (err) {
+                    console.log('Success!')
+                }
+            });
+        });
 }
 
 // Function call to initialize app
